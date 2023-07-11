4eb297b386ccb0f4923da79e9c4fa4deb1e0f737
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2023-07-12 00:00:36 +0300

    Comment out not build endpoints

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/internal/operators';
+import { catchError, map } from 'rxjs/operators';
 import {
   HttpClient,
   HttpHeaders,
   HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://mymovieapp.herokuapp.com/';
 @Injectable({
   providedIn: 'root',
 })
@@ -34,35 +33,35 @@ export class apiService {
       .pipe(catchError(this.handleError));
   }
 
-  // Making the api call for the get all movies endpoint
-  getAllMovies(): Observable<any> {}
+  // // Making the api call for the get all movies endpoint
+  // getAllMovies(): Observable<any> {}
 
-  // Making the api call for the get one movie endpoint
-  getOneMovie(title: string): Observable<any> {}
+  // // Making the api call for the get one movie endpoint
+  // getOneMovie(title: string): Observable<any> {}
 
-  // Making the api call for the get one director endpoint
-  getDirector(directorName: string): Observable<any> {}
+  // // Making the api call for the get one director endpoint
+  // getDirector(directorName: string): Observable<any> {}
 
-  // Making the api call for the get one genre endpoint
-  getGenre(genreName: string): Observable<any> {}
+  // // Making the api call for the get one genre endpoint
+  // getGenre(genreName: string): Observable<any> {}
 
-  // Making the api call for the get one user endpoint
-  getUser(): Observable<any> {}
+  // // Making the api call for the get one user endpoint
+  // getUser(): Observable<any> {}
 
-  // Making the api call for the get favourite movies for a user endpoint
-  getFavoriteMovies(): Observable<any> {}
+  // // Making the api call for the get favourite movies for a user endpoint
+  // getFavoriteMovies(): Observable<any> {}
 
-  // Making the api call for the add a movie to favourite Movies endpoint
-  addFavoriteMovie(movieId: string): Observable<any> {}
+  // // Making the api call for the add a movie to favourite Movies endpoint
+  // addFavoriteMovie(movieId: string): Observable<any> {}
 
-  // Making the api call for the edit user endpoint
-  editUser(updatedUser: any): Observable<any> {}
+  // // Making the api call for the edit user endpoint
+  // editUser(updatedUser: any): Observable<any> {}
 
-  // Making the api call for the delete user endpoint
-  deleteUser(): Observable<any> {}
+  // // Making the api call for the delete user endpoint
+  // deleteUser(): Observable<any> {}
 
-  // Making the api call for the elete a movie from the favorite movies endpoint
-  deleteFavoriteMovie(movieId: string): Observable<any> {}
+  // // Making the api call for the elete a movie from the favorite movies endpoint
+  // deleteFavoriteMovie(movieId: string): Observable<any> {}
 
   private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
