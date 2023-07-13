f03f8a80af3501b25725d8402de352c4b97018e5
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2023-07-14 01:17:30 +0300

    correct module name

--- src/app/fetch-api-data.service.spec.ts
+++ src/app/fetch-api-data.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FetchApiDataService } from './fetch-api-data.service';
+import { apiService } from './fetch-api-data.service';
 
-describe('FetchApiDataService', () => {
-  let service: FetchApiDataService;
+describe('apiService', () => {
+  let service: apiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FetchApiDataService);
+    service = TestBed.inject(apiService);
   });
 
   it('should be created', () => {
